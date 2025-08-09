3fc86ae4aa8c9b7631c8a69fd8a56f7055727f42
Mayke <mayke000000000@gmail.com>
2025-08-09 10:08:52 -0300

    chore: adicionar docker-compose e arquivo .env para configuração do ambiente

--- server/src/database/queries.ts
+++ server/src/database/queries.ts
@@ -1,11 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "senha",
-  database: process.env.DB_NAME || "meubanco",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) ,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 /**
