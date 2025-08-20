3183de3e4da2053e98b21140463c38b768be379f
Mayke <mayke000000000@gmail.com>
2025-08-20 13:05:54 -0300

    refac: tabelas

--- server/src/database/table.ts
+++ server/src/database/table.ts
@@ -19,8 +19,9 @@ CREATE TABLE IF NOT EXISTS pessoa (
        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS ORGAO_EXTERNO (
+  CREATE TABLE IF NOT EXISTS ORGAO_EXTERNO (
     ID_Orgao SERIAL PRIMARY KEY,
+    ID_Doador INTEGER NOT NULL ,
     Nome_oficial VARCHAR(255) NOT NULL,
     CNPJ VARCHAR(18) UNIQUE NOT NULL,
     Responsavel VARCHAR(255),
@@ -173,18 +174,18 @@ CREATE TABLE IF NOT EXISTS versao_fisica (
 
 CREATE TABLE IF NOT EXISTS EMPRESTIMO (
     ID_Usuario INTEGER NOT NULL,
-    ID_Publicacao INTEGER NOT NULL,
+    id_exemplar INTEGER NOT NULL,
     Data_emprestimo TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     Data_prevista_devolucao DATE NOT NULL,
     Data_real_devolucao TIMESTAMP WITH TIME ZONE,
     Status VARCHAR(10) NOT NULL DEFAULT 'ATIVO' CHECK (Status IN ('ATIVO', 'DEVOLVIDO', 'ATRASADO')),
     Valor_multa NUMERIC(10, 2) DEFAULT 0.00,
-    CONSTRAINT pk_emprestimo PRIMARY KEY (ID_Usuario, ID_Publicacao, Data_emprestimo),
+    CONSTRAINT pk_emprestimo PRIMARY KEY (ID_Usuario,id_exemplar, Data_emprestimo),
     CONSTRAINT fk_emprestimo_usuario
       FOREIGN KEY (ID_Usuario) REFERENCES USUARIO(id_pessoa)
       ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_emprestimo_publicacao
-      FOREIGN KEY (ID_Publicacao) REFERENCES PUBLICACAO(ID_Publicacao)
+      FOREIGN KEY (id_exemplar) REFERENCES EXEMPLAR(id_exemplar)
       ON DELETE RESTRICT ON UPDATE CASCADE
   );
 `
